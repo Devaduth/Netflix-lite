bea0b9c3c98e71b8dda78a0040ab28210d6feea2
Devaduth <kmdevaduth@gmail.com>
2022-07-15 13:42:02 +0530

    final commit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,13 +5,11 @@ function NavBar() {
 
     return (
         <div>
-
             <div className="navbar">
                 <img className='logo'
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
                     alt="" />
                 <h1  style={{color:'rgb(252, 28, 28)'}}>Trendings</h1>
-
             </div>
         </div>
 
